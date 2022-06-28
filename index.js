1b0510779c3fe9dc3cb6205d2a42c8c54662101d
Chris Jackson <chrisjackson@Chriss-MBP.fios-router.home>
2022-06-28 00:23:42 -0400

    edits to my homework

--- index.js
+++ index.js
@@ -1,47 +1,24 @@
 //Imports package inquirer
 const inquirer = require('inquirer');
 
+function createOptions(){
 inquirer
     .prompt([
         {
             type: "list",
             name: 'options',
-            message: 'Choose an option below: '
-            choices: ["View all departments", "View all Roles", "View all Employees"]
-        },
-        {
-            type: "input",
-            name: 'roles',
-            message: 'View all roles'
-        },
-        {
-            type: "input",
-            name: 'employees',
-            message: 'View all employees'
-        },
-
-        {
-            type: "input",
-            name: 'employees',
-            message: 'Add a department'
-        },
-
-        {
-            type: "input",
-            name: 'employees',
-            message: 'View all employees'
-        },
-        {
-            type: "List",
-            name: 'employees',
-            message: 'View all employees'
-        },
+            message: 'View all roles',
+            choices: ['View All Roles', 'View All Employees','View All Departments','Add Role','Add Department','Add Employee','Update Employee Role']
+        }
     ])
     .then(answers => {
-        console.info(answers);
-        console.info(answers.departments);
-        console.info(answers.roles);
-        console.info(answers.employees);
-        if(answers === )
+        //set up a switch case,
+        //in the case of view all roles, then we'll go ahead an make a view all roles SQL QUery
+        //in the case
+      const departments = new Departments(answers.departments, answer.roles, answer.employees) 
+        console.log(departments);
+        //if(answers === )
     });
+}
 
+createOptions();
---
+++ seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (name)
+VALUES ("Tech"),
+       ("Sales"),
+       ("HR"),
+       ("Marketing"),
+       ("Finance");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Senior-Tech", 100000,  1),
+        ("Sales-CEO", 10000, 2),
+        ("HR-Manager", 100000, 3),
+        ("Marking-Ceo", 100000, 4),
+        ("Finance-Junior", 100000, 5)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("MARK", "LANE", 2, NULL),
+       ("Marcus", "Lane", 1, NULL),
+       ("Matthew", "Lane", 1, 1),
+       ("Casey", "Lane", 1, NULL),
+       ("John", "Lane", 1, NULL)
+       
+
+      
